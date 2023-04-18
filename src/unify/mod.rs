0711da827061420cafd5038558ec6631b76474c8
Thaumy <thaumy@outlook.com>
2023-04-18 18:58:01 +0800

    adjust Lift debug output

--- src/unify/mod.rs
+++ src/unify/mod.rs
@@ -21,10 +21,7 @@ pub fn lift(
     from: &Type,
     to: &Type
 ) -> Option<Type> {
-    println!(
-        "{:8}{:>10} │ <{from:?}> to <{to:?}>",
-        "[unify]", "Lift"
-    );
+    println!("{:8}{:>10} │ {from:?} to {to:?}", "[unify]", "Lift");
 
     if let Type::NamelyType(n) = from
         && !type_env.exist_ref(n) { return None; }
